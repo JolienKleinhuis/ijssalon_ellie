d456699bafe6214e7b1f4532f1c37982e7476aa2
Jolien Kleinhuis <j.kleinhuis@pento.nl>
2023-05-11 13:56:50 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,22 +1,28 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-aanbieding = prijzen["vanille"]*0.8
+from helper import decoreer
 
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    aanbieding = prijzen["vanille"]*0.8
 
-reclame_tekst2=reclame_tekst[:62]
+    reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
 
-reclame_tekst3=reclame_tekst2.upper()
+    reclame_tekst2=reclame_tekst[:62]
 
-reclame_tekst4=reclame_tekst3.split(" ")
+    reclame_tekst3=reclame_tekst2.upper()
 
-print()
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el)
-    else:
-        print(el.lower())
-    
+    reclame_tekst4=reclame_tekst3.split(" ")
+
+    print()
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el)
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
+        
